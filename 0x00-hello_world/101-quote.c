0e014222f5c544053e0acffd6bb5ead9913270b3
Nemutaduni Dakalo <nemutadunid@gmail.com>
2023-08-12 03:39:36 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point that prints a line to the standard error
+ * Return: returns 1
+ */
+int main(void)
+{
+
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+
+
+return (1);
+}
