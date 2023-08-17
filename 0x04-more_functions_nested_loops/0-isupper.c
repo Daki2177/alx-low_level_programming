7c0f8685efe59fd4a9bf4d7c2732d32e97fb5f27
Nemutaduni Dakalo <nemutadunid@gmail.com>
2023-08-17 03:39:58 -0700

    unction that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check if letter is upper
+ * @n: number to be computed
+ * Return: returns 1 for upper and 0 for anything else
+ */
+
+int _isupper(int n)
+{
+if (n >= 65 && n <= 90)
+{
+return (1);
+}
+return (0);
+}
