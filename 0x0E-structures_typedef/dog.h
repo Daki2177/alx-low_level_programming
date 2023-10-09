ddc928dfd2dd48a75b533d9aae0febed0c2cdd7b
Nemutaduni Dakalo <nemutadunid@gmail.com>
2023-10-09 06:05:48 -0700

    Define a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - Dog attributes
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: The attributes of the dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
